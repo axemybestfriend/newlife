0942406baa20648370edabd4220e9a161965cd03
axemybestfriend <axemybestfriend@mail.ru>
2024-01-10 10:34:39 +0700

    13

--- minesweeper/minesweeper/mainform.h
+++ minesweeper/minesweeper/mainform.h
@@ -19,6 +19,7 @@ namespace minesweeper {
 	public ref class mainform : public System::Windows::Forms::Form
 	{
 	public:
+		int seconds = 0;
 		mainform(void)
 		{
 			InitializeComponent();
@@ -30,6 +31,7 @@ namespace minesweeper {
 					this->Controls->Add(arr[i][j]);
 				}
 			}
+			timer1->Start();
 		}
 
 	protected:
@@ -43,11 +45,14 @@ namespace minesweeper {
 				delete components;
 			}
 		}
-	private:
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::Windows::Forms::Label^ Time;
+
+	private: System::ComponentModel::IContainer^ components;
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -56,17 +61,47 @@ namespace minesweeper {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->Time = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Interval = 1000;
+			this->timer1->Tick += gcnew System::EventHandler(this, &mainform::timer_Tick);
+			// 
+			// Time
+			// 
+			this->Time->AutoSize = true;
+			this->Time->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Time->ForeColor = System::Drawing::Color::Red;
+			this->Time->Location = System::Drawing::Point(445, 9);
+			this->Time->Name = L"Time";
+			this->Time->Size = System::Drawing::Size(43, 24);
+			this->Time->TabIndex = 0;
+			this->Time->Text = L"000";
 			// 
 			// mainform
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(500, 500);
+			this->Controls->Add(this->Time);
 			this->Name = L"mainform";
 			this->Text = L"mainform";
 			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void timer_Tick(System::Object^ sender, System::EventArgs^ e) {
+		seconds++;
+		if (seconds < 10)Time->Text = "00" + Convert::ToString(seconds);
+		else if (seconds < 100)Time->Text = "0" + Convert::ToString(seconds);
+		else if (seconds < 1000)Time->Text = Convert::ToString(seconds);
+		else Text = "999";
+	}
 	};
 }
